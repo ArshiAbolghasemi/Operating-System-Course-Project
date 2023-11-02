8c72c998e7cba3ddc388dfa203131ee82367ced9
a.abolghasemi <a.abolghasemi@digikala.com>
2023-11-02 15:48:47 +0330

    fix

--- socket-programming/src/util/util.c
+++ socket-programming/src/util/util.c
@@ -3,6 +3,6 @@
 char* color(char* text, AnsiColor color)
 {
     char* temp = (char*)malloc(256);
-    sprintf(temp, "\x1B[%dm%s\x1B[%dm\n", color, text, DEFAULT);
+    sprintf(temp, "\x1B[%dm%s\x1B[%dm", color, text, DEFAULT);
     return temp;
 }
