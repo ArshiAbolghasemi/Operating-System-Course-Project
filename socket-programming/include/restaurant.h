531727753fe75159486d8f7d75ddb14698532a5a
a.abolghasemi <a.abolghasemi@digikala.com>
2023-11-03 12:29:20 +0330

    adding open command

--- socket-programming/include/restaurant.h
+++ socket-programming/include/restaurant.h
@@ -3,8 +3,18 @@
 
 #include "user.h"
 
+char cmd[BUFFER_SIZE];
+char msg[BUFFER_SIZE];
+
 struct {
     struct user user;
+    int is_opened;
 } restaurant;
+
+void open_restaurant();
+
+int command();
+
+void run();
 
 #endif
--- socket-programming/src/restaurant.c
+++ socket-programming/src/restaurant.c
@@ -1,21 +1,59 @@
 #include "../include/restaurant.h"
 
+void open_restaurant()
+{
+    restaurant.is_opened = 1;
+    
+    memset(msg, 0, BUFFER_SIZE);
+    sprintf(msg, "%s Restaurant %s!\n", 
+        color((char* )restaurant.user.username, YELLOW), color("opened", GREEN));   
+    if (sendto(restaurant.user.broadcast_fd, msg, strlen(msg), 0,
+            (struct sockaddr *)&restaurant.user.bc_address, 
+            sizeof(restaurant.user.bc_address)) < 0) {
+        error("failed to send opern message\n");        
+    }
+}
+
+int command()
+{
+    memset(cmd, 0, BUFFER_SIZE);
+    read(STDIN_FILENO, cmd, BUFFER_SIZE);
+    trim_white_space_left(cmd);
+    
+    if (strcmp(cmd, "start working")) {
+        open_restaurant();
+    } else if (cmd == '\0') {
+        return EXIT_FAILURE;
+    }
+    
+    return EXIT_FAILURE;
+}
+
 void run()
 {
-    fd_set workingSet;
-    FD_ZERO(&masterSet);
-    FD_ZERO(&workingSet);
-    FD_SET(STDIN_FILENO, &masterSet);
-    FD_SET(restaurant.user.broadcast_fd, &masterSet);
-    maxFd = restaurant.user.broadcast_fd;
-    logInfo("Looking for advertisements...");
+    open_restaurant();
+    fd_set master_set, working_set;
+    FD_ZERO(&master_set);
+    FD_ZERO(&working_set);
+    FD_SET(STDIN_FILENO, &master_set);
+    FD_SET(restaurant.user.broadcast_fd, &master_set);
+    int max_fd = restaurant.user.broadcast_fd;
     while (1) {
-        workingSet = masterSet;
+        working_set = master_set;
         propmt();
-        if (select(maxFd + 1, &workingSet, NULL, NULL, NULL) < 0) 
+
+        if (select(max_fd + 1, &working_set, NULL, NULL, NULL) < 0) {
             error("failed in select process");
-        else if (!handleEvent(&workingSet))
-            break;
+            return;
+        }
+
+        if (FD_ISSET(STDIN_FILENO, &working_set)) {
+            int result = command();
+            if (result == EXIT_FAILURE) {
+                break;
+            }
+        }
+        break;
     }
 }
 
