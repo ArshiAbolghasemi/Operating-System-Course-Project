ba43aa600eb567c31bd4ad2928cafd05779dd68b
a.abolghasemi <a.abolghasemi@digikala.com>
2023-11-25 03:19:27 +0330

    adding two new color

--- process-management/include/util/colors.hpp
+++ process-management/include/util/colors.hpp
@@ -10,6 +10,8 @@ namespace colors
     inline std::string Red    = isatty(fileno(stdout)) ? "\033[31m" : "";
     inline std::string Green  = isatty(fileno(stdout)) ? "\033[32m" : "";
     inline std::string Yellow = isatty(fileno(stdout)) ? "\033[33m" : "";
+    inline std::string MAGENTA = isatty(fileno(stdout)) ? "\033[35m" : ""; 
+    inline std::string CYAN = isatty(fileno(stdout)) ? "\033[36m" : "";
 }
 
 #endif
