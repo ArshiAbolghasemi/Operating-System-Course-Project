b18e2e3674b90818ef8d75519dee76663cd03fd1
a.abolghasemi <a.abolghasemi@digikala.com>
2023-11-25 09:44:40 +0330

    remove main

--- process-management/src/utilities_calculator_engine.cpp
+++ process-management/src/utilities_calculator_engine.cpp
@@ -49,17 +49,3 @@ int UtilitiesCalculatorEngine::run()
     this->printBuildings();
     return EXIT_SUCCESS;
 }
-
-int main(int argc, char* argv[])
-{
-    try {
-        UtilitiesCalculatorEngine* calculatorEngine = new UtilitiesCalculatorEngine(argc, argv);
-        calculatorEngine->run();
-        delete calculatorEngine;
-    } catch (const std::exception& exception) {
-        std::cerr << "exception: " << exception.what() << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    return EXIT_SUCCESS;
-}
