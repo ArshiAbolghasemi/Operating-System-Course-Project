551f59041ba0d4be024b3a2aed298379570d4a77
a.abolghasemi <a.abolghasemi@digikala.com>
2023-11-27 11:23:34 +0330

    adding map slice

--- process-management/include/util/util.hpp
+++ process-management/include/util/util.hpp
@@ -10,6 +10,8 @@ private:
 public:
     template <typename T, typename V>
     static std::vector<std::map<T, V>> mapChunk(const std::map<T, V>& map, int chunkSize);
+    template <typename T, typename V>
+    static std::map<T, V> mapSlice(const std::map<T, V>& map, int offset, int limit);
 };
 
 #endif
--- process-management/src/util/util.cpp
+++ process-management/src/util/util.cpp
@@ -1,7 +1,33 @@
 #include "../../include/util/util.hpp"
 
 template <typename T, typename V>
-std::vector<std::map<T, V>> Util::mapChunk(std::map<T, V>& map, int chunkSize)
+std::vector<std::map<T, V>> Util::mapChunk(const std::map<T, V>& map, int chunkSize)
 {
+    std::vector<std::map<T, V>> result;
 
+    auto it = inputMap.begin();
+    while (it != inputMap.end()) {
+        std::map<T, V> chunk;
+        for (std::size_t i = 0; i < chunkSize && it != inputMap.end(); ++i, ++it) {
+            chunk.insert(*it);
+        }
+        result.push_back(chunk);
+    }
+
+    return result;
+}
+
+template <typename T, typename V>
+std::map<T, V> Util::mapSlice(const std::map<T, V>& map, int offset, int limit)
+{
+    auto it = map.begin();
+    while (--offset) ++it;
+
+    std::map<T, V> result;
+    int cnt = 0;
+    for (it; it != map.end() && cnt != limit; ++cnt, ++it) {
+        result.insert(*it);
+    }
+    
+    return result;
 }
