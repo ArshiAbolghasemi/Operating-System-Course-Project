822a39bbf18335c99617480270a38572e4197e49
a.abolghasemi <a.abolghasemi@digikala.com>
2023-12-18 00:53:31 +0330

    addin load pixels

--- multi-thread/lib/include/bmp.hpp
+++ multi-thread/lib/include/bmp.hpp
@@ -4,8 +4,10 @@
 #include <string>
 #include <fstream>
 #include <exception>
+#include <vector>
 
 #include "bmp_header.hpp"
+#include "pixel.hpp"
 
 class BMP
 {
@@ -13,8 +15,11 @@ private:
     std::ifstream* file;
     BMPHeader* header;
     BMPInfoHeader* infoHeader;
+    std::vector<Pixel*> pixels;
 
     void loadFile();
+    void loadHeaders();
+    void loadPixels();
 public:
     explicit BMP(const std::string& fileName);
     ~BMP();
--- multi-thread/lib/src/bmp.cpp
+++ multi-thread/lib/src/bmp.cpp
@@ -11,6 +11,7 @@ BMP::BMP(const std::string& fileName)
 
     this->file = &file;
     this->loadFile();
+    this->file->close();
 }
 
 BMP::~BMP() 
@@ -18,20 +19,38 @@ BMP::~BMP()
     delete this->header;
     delete this->infoHeader;
     delete this->file;
+    for (auto pixel : this->pixels) {
+        delete pixel;
+    }
 }
 
-void BMP::loadFile()
+void BMP::loadHeaders()
 {
-    this->file->read(reinterpret_cast<char*>(&this->header), sizeof(BMPHeader));
+    this->header = new BMPHeader;
+    this->file->read(reinterpret_cast<char*>(this->header), sizeof(BMPHeader));
     if (this->header->signature != BMP_SIGNATURE) {
         throw std::runtime_error("Invalid BMP file");
     }
 
-    this->file->read(reinterpret_cast<char*>(&this->infoHeader), sizeof(BMPInfoHeader));
+    this->infoHeader = new BMPInfoHeader;
+    this->file->read(reinterpret_cast<char*>(this->infoHeader), sizeof(BMPInfoHeader));
 
     if (this->infoHeader->bitsPerPixel != NUMBER_OF_BITS_PER_PIXEL) {
         char errMsg[40];
         sprintf(errMsg, "Only %d-bit BMP files are supported!", NUMBER_OF_BITS_PER_PIXEL);
         throw std::runtime_error(errMsg);
     }
+}
+
+void BMP::loadPixels()
+{
+    std::size_t pixelDataSize = this->infoHeader->width * this->infoHeader->height;
+    this->pixels.resize(pixelDataSize);
+    this->file->read(reinterpret_cast<char*>(pixels.data()), pixelDataSize * sizeof(Pixel));
+}
+
+void BMP::loadFile()
+{
+    this->loadHeaders();
+    this->loadPixels();
 }
